04eec3a03a4d40017302ec88bb99e72dc9dbf694
likeinlife <likeinlife@outlook.com>
2024-08-16 11:06:41 +0300

    minor(tests): test pydantic with qlighttype

--- tests/test_with_pydantic.py
+++ tests/test_with_pydantic.py
@@ -1,5 +1,5 @@
 import pytest
-from light_types import LightType
+from light_types import LightType, QLightType, StringQ
 from pydantic import BaseModel, TypeAdapter
 
 
@@ -8,6 +8,22 @@ def test_with_pydantic() -> None:
         @classmethod
         def validate(cls, value: str) -> bool:
             return value.startswith("String")
+
+    class MyModel(BaseModel):
+        value: StartsWithString
+
+    assert TypeAdapter(MyModel).validate_python({"value": "StringOk"})
+
+    with pytest.raises(TypeError):
+        TypeAdapter(MyModel).validate_python({"value": "InvalidString"})
+
+    with pytest.raises(TypeError):
+        TypeAdapter(MyModel).validate_python({"value": 400})
+
+
+def test_with_pydantic_qlighttype() -> None:
+    class StartsWithString(str, QLightType):
+        validator = StringQ().startswith("String")
 
     class MyModel(BaseModel):
         value: StartsWithString
